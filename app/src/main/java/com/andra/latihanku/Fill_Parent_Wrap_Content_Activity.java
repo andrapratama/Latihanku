04a62a6b7ccfb8045859c47638874bedddc1f79b
Andra Satria Pratama <andrasatriapratama@gmail.com>
2016-12-09 16:18:13 +0700

    Latihan Program Android

---
+++ app/src/main/java/com/andra/latihanku/Fill_Parent_Wrap_Content_Activity.java
@@ -0,0 +1,14 @@
+package com.andra.latihanku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fill_Parent_Wrap_Content_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill__parent__wrap__content_);
+
+    }
+}
---
+++ app/src/main/java/com/andra/latihanku/MainActivity.java
@@ -0,0 +1,42 @@
+package com.andra.latihanku;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.button1);
+        btn2 = (Button)findViewById(R.id.button2);
+        btn3 = (Button)findViewById(R.id.button3);
+        btn4 = (Button)findViewById(R.id.button4);
+        btn5 = (Button)findViewById(R.id.button5);
+        btn6 = (Button)findViewById(R.id.button6);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Fill_Parent_Wrap_Content_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Linear_Layout.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
