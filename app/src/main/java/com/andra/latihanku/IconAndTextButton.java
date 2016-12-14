a31841c4224ad26950c60bf16b29be7681e58b5d
Andra Satria Pratama <andrasatriapratama@gmail.com>
2016-12-14 10:24:50 +0700

    Latihan Program Android

---
+++ app/src/main/java/com/andra/latihanku/IconAndTextButton.java
@@ -0,0 +1,13 @@
+package com.andra.latihanku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IconAndTextButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icon_and_text_button);
+    }
+}
---
+++ app/src/main/java/com/andra/latihanku/ScrollViewVertical.java
@@ -0,0 +1,13 @@
+package com.andra.latihanku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollViewVertical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view_vertical);
+    }
+}
